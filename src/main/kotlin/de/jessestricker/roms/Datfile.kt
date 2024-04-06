524ed7dcc4f6aba29b7775d3b28e5fce26cca0c5
Jesse Stricker <git@jessestricker.de>
2024-04-06 16:31:06 +0200

    refactor: move serialization config from annotations to mapper

--- src/main/kotlin/de/jessestricker/roms/Datfile.kt
+++ src/main/kotlin/de/jessestricker/roms/Datfile.kt
@@ -1,58 +1,53 @@
 package de.jessestricker.roms
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.kotlinModule
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
-
-internal val xmlMapper = XmlMapper().apply {
-    registerKotlinModule { enable(KotlinFeature.StrictNullChecks) }
-    enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
-}
 
 data class Datfile(
     val header: Header,
 
-    @JacksonXmlProperty(localName = "game")
-    @JacksonXmlElementWrapper(useWrapping = false)
+    @field:JacksonXmlProperty(localName = "game")
     val games: Set<Game>,
 ) {
     companion object {
+        private val xmlMapper = XmlMapper.builder()
+            .addModule(kotlinModule { enable(KotlinFeature.StrictNullChecks) })
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
+            .defaultUseWrapper(false)
+            .build()
+
         fun load(file: File): Datfile {
             return xmlMapper.readValue(file)
         }
     }
 }
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Header(
     val name: String,
     val version: String,
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Game(
-    @JacksonXmlProperty(isAttribute = true)
+    @field:JacksonXmlProperty(isAttribute = true)
     val name: String,
 
-    @JacksonXmlProperty(localName = "rom")
-    @JacksonXmlElementWrapper(useWrapping = false)
+    @field:JacksonXmlProperty(localName = "rom")
     val roms: Set<Rom>,
 )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class Rom(
-    @JacksonXmlProperty(isAttribute = true)
+    @field:JacksonXmlProperty(isAttribute = true)
     val name: String,
 
-    @JacksonXmlProperty(isAttribute = true)
+    @field:JacksonXmlProperty(isAttribute = true)
     val size: Long,
 
-    @JacksonXmlProperty(isAttribute = true)
+    @field:JacksonXmlProperty(isAttribute = true)
     val sha1: String,
 )
